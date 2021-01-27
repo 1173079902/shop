7533eb9a3c8b5eed33cd9ed3d9f9177b2ca7ee35
1173079902 <1173079902@qq.com>
2021-01-27 16:40:26 +0800

    06-为文本框添加图标

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import './assets/fonts/iconfont.css'
 //导入全局样式
 import './assets/css/global.css'
 
