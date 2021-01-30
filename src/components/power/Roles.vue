142c4d9c8bacc064f221424c0f7fd6994d1eeaad
1173079902 <1173079902@qq.com>
2021-01-30 19:29:51 +0800

    52_获取角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 添加角色按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
-    
+  data() {
+    return {
+      // 权限列表
+      rolesList: []
+    }
+  },
+  created() {},
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) return this.$message.error('获取角色列表失败')
+      this.rolesList = res.data
+    }
+  }
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
