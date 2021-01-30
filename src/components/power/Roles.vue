5508820c96aeb96008c4e4fd1394064eaad26349
1173079902 <1173079902@qq.com>
2021-01-30 19:38:44 +0800

    53_渲染角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -14,6 +14,21 @@
           <el-button type="primary">添加角色</el-button>
         </el-col>
       </el-row>
+      <el-table :data="roleList" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand"> </el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index" label="#"> </el-table-column>
+        <el-table-column prop="roleName" label="角色名称"> </el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"> </el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template>
+            <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+            <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+            <el-button type="warning" size="mini" icon="el-icon-setting">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -22,15 +37,17 @@ export default {
   data() {
     return {
       // 权限列表
-      rolesList: []
+      roleList: []
     }
   },
-  created() {},
+  created() {
+    this.getRolesList()
+  },
   methods: {
     async getRolesList() {
       const { data: res } = await this.$http.get('roles')
       if (res.meta.status !== 200) return this.$message.error('获取角色列表失败')
-      this.rolesList = res.data
+      this.roleList = res.data
     }
   }
 }
