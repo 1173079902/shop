1e3962b46b5ac5d1e3d73998f04ec17d4e78df6f
1173079902 <1173079902@qq.com>
2021-01-31 16:14:21 +0800

    70_初步使用vue-table-with-tree-grid

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './assets/fonts/iconfont.css'
 //导入全局样式
 import './assets/css/global.css'
 
+import TreeTable from 'vue-table-with-tree-grid'
+
 //导入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -20,6 +22,8 @@ axios.interceptors.response.use((res) => {
   return res
 })
 Vue.prototype.$http = axios
+
+Vue.component('tree-table', TreeTable)
 
 Vue.config.productionTip = false
 
