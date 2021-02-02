77d9bf1fe5ac9e94708167bf0a71d4227ebac497
1173079902 <1173079902@qq.com>
2021-02-02 16:19:09 +0800

    89_实现newTag按钮切换文本框的功能

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -28,7 +28,12 @@
             <!-- 展开行的操作 -->
             <el-table-column type="expand">
               <template slot-scope="scope">
+                <!-- 循环渲染 Tag 标签 -->
                 <el-tag v-for="(item, i) in scope.row.attr_vals" :key="i" closable>{{ item }}</el-tag>
+                <!-- 输入的文本框 -->
+                <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm"></el-input>
+                <!-- 添加的按钮 -->
+                <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
               </template>
             </el-table-column>
             <!-- 索引列 -->
@@ -127,7 +132,11 @@ export default {
       editForm: {},
       editFormRules: {
         attr_name: [{ required: true, message: '请输入参数名称', trigger: 'blur' }]
-      }
+      },
+      // 控制按钮与文本框的切换显示
+      inputVisible: false,
+      // 文本框中输入的内容
+      inputValue: ''
     }
   },
   created() {
@@ -219,7 +228,8 @@ export default {
         if (!valid) return false
         const { data: res } = await this.$http.put(`categories/${this.cateId}/attributes/${this.editForm.attr_id}`, {
           attr_name: this.editForm.attr_name,
-          attr_sel: this.activeName
+          attr_sel: this.activeName,
+          attr_vals: this.editForm.attr_vals //把之前的带上，防止把之前的清空掉
         })
         if (res.meta.status !== 200) {
           return this.$message.error('修改参数失败')
@@ -245,6 +255,12 @@ export default {
       }
       this.$message.success('删除参数成功')
       this.getParamsData()
+    },
+    // 文本框失去焦点或 Enter
+    handleInputConfirm() {},
+    // 点击按钮展示输入文本框
+    showInput() {
+      this.inputVisible = true
     }
   },
   computed: {
@@ -277,5 +293,11 @@ export default {
 <style lang="less" scoped>
 .cat_opt {
   margin: 15px 0;
+}
+.el-tag {
+  margin: 0 10px;
+}
+.input-new-tag {
+  width: 120px;
 }
 </style>
