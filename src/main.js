356df7ffe0fa32cd94cc3d82d1697ab8b4096f0c
1173079902 <1173079902@qq.com>
2021-02-05 16:47:12 +0800

    107_项目优化

--- src/main.js
+++ src/main.js
@@ -16,11 +16,22 @@ import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 //全局注册富文本组件
 Vue.use(VueQuillEditor)
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 //导入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use((config) => {
+  // 展示进度条
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在 response 拦截器中，隐藏进度条
+axios.interceptors.response.use((config) => {
+  NProgress.done()
   return config
 })
 axios.interceptors.response.use((res) => {
