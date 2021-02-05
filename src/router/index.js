1aad203c864842271a2dd6b900414dd34ebb670d
1173079902 <1173079902@qq.com>
2021-02-05 13:55:44 +0800

    103_订单列表路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 //注册路由组件
 Vue.use(VueRouter)
 
@@ -31,11 +32,8 @@ const router = new VueRouter({
         { path: '/categories', component: Cate },
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList },
-        {
-          // 注意这种写法和直接把 /add 当做 /goods 的子路由是不一样的
-          path: '/goods/add',
-          component: Add
-        }
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order }
       ]
     }
   ]
